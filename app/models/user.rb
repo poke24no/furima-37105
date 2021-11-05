4fa8e1910dadc9110dcf834615ff104034b49776
poke24no <mopn@icloud.com>
2021-11-05 17:27:59 +0900

    新規登録ページのフォームにカラムを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :name,               presence: true
-         validates :email,              presence: true, unique: true
+         validates :email,              presence: true
          validates :encrypted_password, presence: true
          validates :birthday,           presence: true
          validates :first_name,         presence: true
