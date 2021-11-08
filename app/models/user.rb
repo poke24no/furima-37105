90a3e10ac601f7dd5e5892331b4d1604b819652b
poke24no <mopn@icloud.com>
2021-11-08 11:22:55 +0900

    emailとpasswordの不要なバリデージョンを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
          validates :name,                presence: true
-         validates :email,               presence: true
-         validates :password,           {presence: true, format: { with: VALID_PASSWORD_REGEX },length:{minimum:6}}
+         validates :password,            format: { with: VALID_PASSWORD_REGEX }
          validates :birthday,            presence: true
          validates :first_name,         {presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }}
          validates :family_name,        {presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }}
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,51 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
-    
-    
+  context '新規登録できる時'
+    it '全ての項目が存在すれば登録できる' do
+      
+    end
+  context '新規登録できない時' do
+    it 'nameが空では登録できない' do
+      
+     end 
+    it 'emailが空では保存できない' do
+      
+     end
+    it 'emailが一意性であること' do
+      
+     end
+    it 'emailには@がないと登録できない' do
+      
+     end
+    it 'passwaordが空では登録できない' do
+      
+     end
+    it 'passwordは6文字以上ないと登録できない' do
+      
+     end
+    it 'passwordとpassword_confirmationが不一致では登録できない' do
+      
+     end
+    it 'passwordには半角英数字混合でないと登録できない' do
+      
+     end
+    it '苗字と名前(全角)が空では登録できない' do
+      
+     end
+    it '苗字と名前(全角)は全角(漢字、ひらがな、カタカナ)でないと登録できない' do
+      
+     end
+    it '苗字と名前(カナ)が空では登録できない' do
+      
+     end
+    it '苗字と名前(カナ)は全角(カタカナ)でないと登録できない' do
+      
+     end
+    it '生年月日が空では登録できない' do
+      
+    end
+  end
   end
 
 end
