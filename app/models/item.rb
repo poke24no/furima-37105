0b7bf0ae056f7666177720e270190496b5bbca21
poke24no <mopn@icloud.com>
2021-11-08 19:45:28 +0900

    ActiveHashモデルを作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,19 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :day
+  belongs_to :load
+  belongs_to :prefecture
+  belongs_to :status
+  has_many :users
+  
+  validates :item_name, presence: true
+  validates :explanation, presence: true
+  validates :price, presence: true
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :day_id, numericality: { other_than: 1 } 
+  validates :load_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :status_id, numericality: { other_than: 1 } 
 end
