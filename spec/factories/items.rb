41cb02a20365bda5ecdc369d5a23dbd1b045d174
poke24no <mopn@icloud.com>
2021-11-09 19:56:31 +0900

    itemモデルの単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name          {'商品'}
+    explanation        {'商品説明'}
+    category_id        {2}
+    status_id          {2}
+    price              {10000}
+    load_id            {2}
+    prefecture_id      {2}
+    day_id             {2}
+    after(:build) do |item|
+      item.image.attach(io: File.open('app/assets/images/flag.png'), filename: 'flag.png')
+    end
   end
 end
