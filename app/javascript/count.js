ee85695c6c564a7476625c63bc5a2f8fed766f14
poke24no <mopn@icloud.com>
2021-11-09 15:20:51 +0900

    小数点を切り捨て

--- app/javascript/count.js
+++ app/javascript/count.js
@@ -2,10 +2,11 @@ function count (){
   const price      = document.getElementById("item-price");
   price.addEventListener("keyup",() =>{
     const commission = document.getElementById("add-tax-price");
-    commission.innerHTML =`${(price.value) * 0.1}`
+    commission.innerHTML = Math.floor(`${(price.value) * 0.1}`)
     const profit     = document.getElementById("profit");
-    profit.innerHTML = `${(price.value) * 0.9}`;
+    profit.innerHTML = `${(price.value) - Math.floor((price.value) * 0.1)}`;
   });
 };
 
 window.addEventListener('load', count);
+
