2cbae2dc5691cabe36dcdc77ab7c372236b2dce1
poke24no <mopn@icloud.com>
2021-11-11 18:28:00 +0900

    マイグレーションを記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items
-  resources :addreses, only[:index, :create]
+  resources :items do
+  resources :addresses, only:[:create]
+  end 
 end
