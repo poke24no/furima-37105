335cc941bcc31de6f7815b740c337a7227d089ea
poke24no <mopn@icloud.com>
2021-11-11 12:39:20 +0900

    編集ページに遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,15 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     
   end
+  
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item = item.update(item_params)
+  end
 
   private
 
