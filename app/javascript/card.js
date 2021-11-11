e920475423dc14ff29c0669618c7cd2a9ffdf151
poke24no <mopn@icloud.com>
2021-11-11 19:47:53 +0900

    フォームの情報を取得

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -3,7 +3,15 @@ const pay = () => {
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order[number]"),
+      cvc: formData.get("order[cvc]"),
+      exp_month: formData.get("order[exp_month]"),
+      exp_year: `20${formData.get("order[exp_year]")}`,
+    };
   });
 };
 
